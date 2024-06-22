bf1aa0b462c7bef1db9e600f98e9747c02269730
BPERRIN <perrin.brandon45@gmail.com>
2024-06-22 11:26:16 +0200

    Lien Discord OpenAI

--- bot.js
+++ bot.js
@@ -25,9 +25,65 @@ client.once('ready', () => {
     console.log('Bot is ready!');
 })
 
+// TODO: Replace mapping threads with database
+const threadMap = {};
+
+const getOpenAiThreadId = (discordThreadId) => {
+    return threadMap[discordThreadId];
+}
+
+const addThreadToMap = (discordThreadId, openAiThreadId) => {
+    threadMap[discordThreadId] = openAiThreadId;
+}
+
+// Loop check openai thread status
+const sleep = (ms) => {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+const terminalStates = ["cancelled", "failed", "completed", "expired"];
+const statusCheckLoop = async (openAiThreadId, runId) => {
+    const run = await openai.beta.threads.runs.retrieve(
+        openAiThreadId,
+        runId
+    );
+
+    if(terminalStates.indexOf(run.status) < 0) {
+        await sleep(1000);
+        return statusCheckLoop(openAiThreadId, runId);
+    }
+    console.log(run);
+
+    return run.status;
+}
+
 // Event on receiving a message
 client.on('messageCreate', async message => {
     if(message.author.bot || !message.content || message.content === '') return;
-    message.reply("Tainted Grail est le meilleur jeu!");
-})
 
+    const discordThreadId = message.channel.id;
+    let openAiThreadId = getOpenAiThreadId(discordThreadId);
+
+    if(!openAiThreadId) {
+        const thread = await openai.beta.threads.create();
+        openAiThreadId = thread.id;
+        addThreadToMap(discordThreadId, openAiThreadId);
+    }
+
+    await openai.beta.threads.messages.create(
+        openAiThreadId,
+        { role: "user", content: message.content }
+    );
+
+    const run = await openai.beta.threads.runs.create(
+        openAiThreadId,
+        { assistant_id: process.env.ASSISTANT_ID}
+    );
+
+    await statusCheckLoop(openAiThreadId, run.id);
+
+    const messages = await openai.beta.threads.messages.list(openAiThreadId);
+    const response = messages.data[0].content[0].text.value;
+
+    message.reply(response);
+})
--- quickstart.md
+++ quickstart.md
@@ -1,6 +1,9 @@
 # npm install
 npm config set registry https://registry.npmjs.org/
 npm -g install npm
+npm i express
+
+# dependancies
 npm install discord.js openai
 npm install dotenv
 npm install -g nodemon
